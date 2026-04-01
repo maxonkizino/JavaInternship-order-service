2998f359057a705a62e6a5908de02b7b4e635e15
maxonkizino <mkizino83@gmail.com>
2026-04-01 12:51:58 +0300

    first commit

---
+++ src/main/java/com/javainternshiporderservice/JavaInternshipOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javainternshiporderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInternshipOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternshipOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javainternshiporderservice/JavaInternshipOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainternshiporderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInternshipOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
